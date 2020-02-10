6d704b379963c6a88bf933fa6eaa686c5470b10c
James Marotta <james.marotta@outlook.com>
2020-02-10 11:45:25 +1100

    added delay

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const say = require("say")
-const S = f => say.speak(f)
+const S = f => say.speak(f, null, 1.75)
 const { speech } = require("./speech")
 const prompt = require("prompt-sync")({ sigint: true })
+const wait = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 const sp = speech(0)
 
@@ -9,49 +12,74 @@ const intro = async () => {
   S(sp("intro"))
 
   console.clear()
-  console.log(
-    "                                                                 "
-  )
-  console.log(
-    "                                                                 "
-  )
-  console.log(
-    "                     Welcome to ATM MACHINE 10000                "
-  )
-  console.log(
-    "                We are here to make your life amazing            "
-  )
-  console.log(
-    "                And definitely not steal your identity           "
-  )
-  console.log(
-    "                                                                 "
-  )
-  console.log(
-    "                I see you are a new bank customer                "
-  )
-  console.log(
-    "                    Please enter your details                    "
-  )
+  console.log("                                                       ")
+  console.log("                                                       ")
+  console.log("            Welcome to ATM MACHINE 10000               ")
+  console.log("         We are here to make your life amazing         ")
+  console.log("            And definitely not steal your identity     ")
+  console.log("                                                       ")
+  console.log("           I see you are a new bank customer           ")
+  console.log("               Please enter your details               ")
 }
 
-const pinControl = () => {
+const pinControl = async () => {
   let pin
   do {
-    pin = prompt("Enter a four digit pin")
-    S("four")
-  } while (pin.length < 4)
-
+    pin = prompt("Enter a four digit pin  ")
+    S("four ")
+  } while (pin.length !== 4)
+  S("pinaccept, keep your Pin Safe!")
+  await wait(800)
   return pin
 }
 
+const choice = async (bal, chances) => {
+  await wait(1200)
+  S(sp("help"))
+
+  let choice
+
+  while (true) {
+    switch (choice) {
+      case "1":
+        S(sp("Balance"))
+        // balance(bal)
+        break
+      case "2":
+        S(sp("deposit"))
+        // deposit(bal)
+        break
+      case "3":
+        S(sp("withdraw"))
+        // withdrawal(bal)
+        break
+      case "4":
+        S(sp("change pin"))
+        // change_pin(chances)
+        break
+      case "q":
+        // quit()
+        break
+      case "h":
+        S(sp("help"))
+      default:
+        // send_help()
+        break
+    }
+  }
+}
+
 const main = async () => {
+  let bal = 100
+  let chances = 3
   // await intro()
-  const name = await prompt(`Name(then push enter):  `)
-  S(`Welcome ${name}, now lets set up a PIN: `)
-  const pin = pinControl()
-  S("pinaccept, keep your Pin Safe!")
+  const name = prompt(`Name(then push enter):  `)
+  S(`Welcome ${name}, now lets set up a PIN:  `)
+
+  const pin = await pinControl()
   console.log("Keep your Pin Safe!")
+
+  await choice(bal, chances)
 }
 
 main()
